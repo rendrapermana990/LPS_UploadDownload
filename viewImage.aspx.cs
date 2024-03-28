7e62cd5a0163ff3248bf0d682457d65991fba950
rendrapermana990 <61962542+rendrapermana990@users.noreply.github.com>
2024-03-28 15:15:09 +0700

    Add files via upload

---
+++ viewImage.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class viewImage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string FilePath = null;
+
+        FilePath = Request.QueryString["fn"];
+
+        if (FilePath != null)
+        {
+            string path = Server.MapPath(FilePath);
+            Image1.ImageUrl = FilePath;
+        }
+
+    }
+}
